0980a90f860923dcbcc7edf367e02822e3e45030
Ramon Ayco Jr <Ramon.AycoJr@infor.com>
2016-04-05 14:00:52 +0800

    added angular, bootstrap to package

---
+++ index-angular.js
@@ -0,0 +1,14 @@
+(function(){
+
+var app = angular.module('taskmap', []);
+
+app.controller('NavController', function () {
+    var nav = this;
+    nav.status = "intial status";
+    nav.user = 'ayo';
+});
+
+
+
+
+})();
