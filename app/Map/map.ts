d6a132eabd2dd77bc0d1275bbccd7df0170bb755
Ayo <mail@ayoayco.com>
2016-05-27 19:30:56 +0800

    typescriptified map.js

--- app/Map/map.ts
+++ app/Map/map.ts
@@ -1,23 +1,69 @@
 module leafletapp{
     
-    var map = L.map('map').setView([51.505, -0.09], 13);
-            
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    var map: L.Map = L.map('map');
+    var userLocation: L.LatLng;
+    var tileLayer: L.TileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: '2016 &copy; Ayo Ayco. All Rights Reserved.',
         maxZoom: 18,
         id: 'ayoayco.njl702a2',
         accessToken: 'pk.eyJ1IjoiYXlvYXljbyIsImEiOiI1YzVic1JvIn0.AnH_wYkYOKMJGmrgGT-A8g'
-    }).addTo(map);
+    });
+ 
+    function getUserLatLng(): L.LatLng{
+        
+        var loc: L.LatLng;
+        var options = {
+            // enableHighAccuracy = should the device take extra time or power to return a really accurate result, or should it give you the quick (but less accurate) answer?  
+            enableHighAccuracy: false, 
+            // timeout = how long does the device have, in milliseconds to return a result?
+            timeout: 5000,  
+            // maximumAge = maximum age for a possible previously-cached position. 0 = must return the current position, not a prior cached position
+            maximumAge: 0 
+        };
+        
+        // call getCurrentPosition()
+        navigator.geolocation.getCurrentPosition(success, error , options);
+        
+        
+        // upon success, do this
+        function success(pos){
+            // get longitude and latitude from the position object passed in
+            console.log("inside success "+loc.lat+","+loc.lng);
+            return loc = L.latLng(pos.coords.latitude,pos.coords.longitude); 
+        }
+        
+        // upon error, do this
+        function error(err){
+            console.log('Error: ' + err + ' :('); // alert the error message
+        }
+        
+        console.log("inside function "+loc.lat+","+loc.lng);
+        return loc;
+    }
 
-    var marker = L.marker([51.5, -0.09]).addTo(map);
+ 
+    if('geolocation' in navigator){
+    // geolocation is supported :)
+        userLocation = getUserLatLng();
+    }else{
+    // no geolocation :(
+        userLocation = L.latLng(51.505, -0.09);
+    } 
 
-    var circle = L.circle([51.508, -0.11], 500, {
+    map.setView([userLocation.lat, userLocation.lng], 13);
+    tileLayer.addTo(map);
+    
+/*
+
+// sample things we can do with leaflet
+
+    var marker: L.Marker = L.marker([51.5, -0.09]);
+    var circle: L.Circle= L.circle([51.508, -0.11], 500, {
         color: 'red',
         fillColor: 'red',
         fillOpacity: 0.4,
-    }).addTo(map);
-
-    var polygon = L.polygon([
+    });
+    var polygon: L.Polygon = L.polygon([
         [51.509, -0.08],
         [51.503, -0.06],
         [51.51, -0.047]
@@ -25,22 +71,28 @@ module leafletapp{
         color: 'blue',
         fillColor: 'red',
         opacity: 0
-    }).addTo(map);
+    });
+    var popup: L.Popup = L.popup();  
+    
+    marker.addTo(map);
+    circle.addTo(map);
+    polygon.addTo(map);
 
     marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
     circle.bindPopup("I am a circle.");
     polygon.bindPopup("I am a polygon.");
 
-    var popup = L.popup()
+    popup
         .setLatLng([51.5, -0.19])
         .setContent("I am a standalone popup.")
         .openOn(map);
 
-    function onMapClick(e) {
+    function onMapClick(e: any) {
         //alert("You clicked the map at " + e.latlng);
         L.marker(e.latlng).addTo(map).bindPopup("hello world!");
     }
 
     map.on('click', onMapClick);
+*/
     
 }
--- scripts/Map/map.js
+++ scripts/Map/map.js
@@ -1,37 +1,46 @@
 var leafletapp;
 (function (leafletapp) {
-    var map = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    var map = L.map('map');
+    var userLocation;
+    var tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: '2016 &copy; Ayo Ayco. All Rights Reserved.',
         maxZoom: 18,
         id: 'ayoayco.njl702a2',
         accessToken: 'pk.eyJ1IjoiYXlvYXljbyIsImEiOiI1YzVic1JvIn0.AnH_wYkYOKMJGmrgGT-A8g'
-    }).addTo(map);
-    var marker = L.marker([51.5, -0.09]).addTo(map);
-    var circle = L.circle([51.508, -0.11], 500, {
-        color: 'red',
-        fillColor: 'red',
-        fillOpacity: 0.4
-    }).addTo(map);
-    var polygon = L.polygon([
-        [51.509, -0.08],
-        [51.503, -0.06],
-        [51.51, -0.047]
-    ], {
-        color: 'blue',
-        fillColor: 'red',
-        opacity: 0
-    }).addTo(map);
-    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-    circle.bindPopup("I am a circle.");
-    polygon.bindPopup("I am a polygon.");
-    var popup = L.popup()
-        .setLatLng([51.5, -0.19])
-        .setContent("I am a standalone popup.")
-        .openOn(map);
-    function onMapClick(e) {
-        //alert("You clicked the map at " + e.latlng);
-        L.marker(e.latlng).addTo(map).bindPopup("hello world!");
+    });
+    function getUserLatLng() {
+        var loc;
+        var options = {
+            // enableHighAccuracy = should the device take extra time or power to return a really accurate result, or should it give you the quick (but less accurate) answer?  
+            enableHighAccuracy: false,
+            // timeout = how long does the device have, in milliseconds to return a result?
+            timeout: 5000,
+            // maximumAge = maximum age for a possible previously-cached position. 0 = must return the current position, not a prior cached position
+            maximumAge: 0
+        };
+        // call getCurrentPosition()
+        navigator.geolocation.getCurrentPosition(success, error /*, options*/);
+        // upon success, do this
+        function success(pos) {
+            // get longitude and latitude from the position object passed in
+            console.log("inside success " + loc.lat + "," + loc.lng);
+            return loc = L.latLng(pos.coords.latitude, pos.coords.longitude);
+        }
+        // upon error, do this
+        function error(err) {
+            console.log('Error: ' + err + ' :('); // alert the error message
+        }
+        console.log("inside function " + loc.lat + "," + loc.lng);
+        return loc;
     }
-    map.on('click', onMapClick);
+    if ('geolocation' in navigator) {
+        // geolocation is supported :)
+        userLocation = getUserLatLng();
+    }
+    else {
+        // no geolocation :(
+        userLocation = L.latLng(51.505, -0.09);
+    }
+    map.setView([userLocation.lat, userLocation.lng], 13);
+    tileLayer.addTo(map);
 })(leafletapp || (leafletapp = {}));
